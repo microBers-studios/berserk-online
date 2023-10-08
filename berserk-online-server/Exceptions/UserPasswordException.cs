7ca70a1a6a8a7a645118fddbe907ecacd009c9f9
ValRar <val21219@gmail.com>
2023-10-08 17:40:56 +0300

    feat: password hashing

---
+++ berserk-online-server/Exceptions/UserPasswordException.cs
@@ -0,0 +1,9 @@
+﻿using System.Data.Common;
+
+namespace berserk_online_server.Exceptions
+{
+    public class UserPasswordException : DbException
+    {
+        public UserPasswordException(string message) : base(message) { }
+    }
+}
--- berserk-online-server/Facades/UsersDatabase.cs
+++ berserk-online-server/Facades/UsersDatabase.cs
@@ -1,18 +1,15 @@
 ﻿using berserk_online_server.Contexts;
+using berserk_online_server.Exceptions;
 using berserk_online_server.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace berserk_online_server.Facades
 {
-    public enum ExistanceStatus
-    {
-        Exists,
-        InvalidPassword,
-        NotExists,
-    }
 
     public class UsersDatabase
     {
         private readonly Databases db;
+        private readonly IPasswordHasher<User> hasher = new PasswordHasher<User>();
         public UsersDatabase(Databases db)
         {
             this.db = db;
@@ -20,6 +17,7 @@ namespace berserk_online_server.Facades
 
         public void AddUser(User user)
         {
+            user.Password = hasher.HashPassword(user, user.Password);
             db.Users.Add(user);
             db.SaveChanges();
         }
@@ -28,20 +26,42 @@ namespace berserk_online_server.Facades
             db.Remove(user);
             db.SaveChanges();
         }
+        public void UpdateUser(User user)
+        {
+            db.Users.Update(user);
+            db.SaveChanges();
+        }
         public bool IsUnique(User user)
         {
             return !db.Users.Any(u => u.Email == user.Email);
         }
-        public ExistanceStatus IsExists(User user)
+        public User FindMatchingUser(User user)
         {
             var matchingUser = db.Users.Where(
                 dbUser => dbUser.Email == user.Email
                 ).Select(dbUser => dbUser).FirstOrDefault();
             if (matchingUser == null)
-                return ExistanceStatus.NotExists;
-            if (matchingUser.Password == user.Password)
-                return ExistanceStatus.Exists;
-            else return ExistanceStatus.InvalidPassword;
+                throw new ArgumentException($"User with email: {user.Email} not found!");
+            if (!isMatchingPasswords(user, matchingUser))
+                throw new UserPasswordException($"User with password: {user.Password} not found!");
+            return matchingUser;
+        }
+        private bool isMatchingPasswords(User providedUser, User dbUser)
+        {
+            switch(hasher.VerifyHashedPassword(dbUser, dbUser.Password, providedUser.Password))
+            {
+                case PasswordVerificationResult.Success: return true;
+                case PasswordVerificationResult.Failed: return false;
+                case PasswordVerificationResult.SuccessRehashNeeded:
+                    rehashUserPassword(dbUser);
+                    return true;
+            }
+            throw new Exception();
+        }
+        private void rehashUserPassword(User user)
+        {
+            user.Password = hasher.HashPassword(user, user.Password);
+            UpdateUser(user);
         }
     }
 }
