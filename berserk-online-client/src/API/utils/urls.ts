542d67e91ccf349c3d9582a0bf16000b31281279
ValRar <val21219@gmail.com>
2023-10-15 22:08:43 +0300

    refactor: remove backend url config

--- berserk-online-client/src/API/utils/urls.ts
+++
@@ -1,7 +0,0 @@
-export const URL = 'https://localhost:7283'
-
-export enum Paths {
-    REGISTRATION = '/Authentication/register',
-    LOGIN = '/Authentication/login',
-    GET_ME = '/User/getMe'
-}
