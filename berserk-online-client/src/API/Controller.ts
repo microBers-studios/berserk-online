0a63a893dda7a8007e421d53c4b12d67360252a3
DonatArtihovich <117632852+DonatArtihovich@users.noreply.github.com>
2023-10-20 00:02:49 +0300

    refactor: refactor response user info interface and fix 401 error

--- berserk-online-client/src/API/Controller.ts
+++ berserk-online-client/src/API/Controller.ts
@@ -1,10 +1,10 @@
 import { IUser } from "src/app/providers/UserProvider/lib/types/types";
-import { IRegistration, ILogin, IResponseInfo } from "./utils/types";
+import { IRegistration, ILogin, IResponseUserInfo } from "./utils/types";
 import { URL, Paths } from "./utils/urls";
 import defaultAvatar from "src/shared/assets/images/default-avatar.jpg"
 
 export default class APIController {
-    static async registrateUser(obj: IRegistration): Promise<IResponseInfo> {
+    static async registrateUser(obj: IRegistration): Promise<IResponseUserInfo> {
         const path = URL + Paths.REGISTRATION
 
         const response: Response = await fetch(path, {
@@ -22,7 +22,7 @@ export default class APIController {
         return { code: response.status, obj: user }
     }
 
-    static async loginUser(object: ILogin): Promise<IResponseInfo> {
+    static async loginUser(object: ILogin): Promise<IResponseUserInfo> {
         const path = URL + Paths.LOGIN
 
         const response: Response = await fetch(path, {
@@ -40,7 +40,7 @@ export default class APIController {
         return { code: response.status, obj }
     }
 
-    static async getMe(): Promise<IResponseInfo> {
+    static async getMe(): Promise<IResponseUserInfo> {
         const path = URL + Paths.GET_ME
 
         const response: Response = await fetch(path, {
@@ -51,6 +51,17 @@ export default class APIController {
             credentials: 'include'
         })
 
+
+        if (response.status === 401) {
+            return {
+                code: response.status,
+                obj: {
+                    id: -1,
+                    message: 'User unauthorized'
+                }
+            }
+        }
+
         const obj = await response.json()
 
         obj.avatarUrl = obj.avatarUrl
@@ -60,7 +71,7 @@ export default class APIController {
         return { code: response.status, obj }
     }
 
-    static async loadAvatar(input: HTMLInputElement): Promise<IResponseInfo> {
+    static async loadAvatar(input: HTMLInputElement): Promise<IResponseUserInfo> {
         const path = URL + Paths.LOAD_AVATAR
 
         const formData = new FormData()
@@ -79,7 +90,7 @@ export default class APIController {
         return { code: response.status, obj }
     }
 
-    static async updateUser(user: Partial<IUser>): Promise<IResponseInfo> {
+    static async updateUser(user: Partial<IUser>): Promise<IResponseUserInfo> {
         const path = URL + Paths.UPDATE_ME
 
         const response = await fetch(path, {
--- berserk-online-client/src/API/utils/types.ts
+++ berserk-online-client/src/API/utils/types.ts
@@ -12,7 +12,7 @@ export interface ILogin {
     rememberMe: boolean;
 }
 
-export interface IResponseInfo {
+export interface IResponseUserInfo {
     code: number;
     obj: IUser | IError;
 }
